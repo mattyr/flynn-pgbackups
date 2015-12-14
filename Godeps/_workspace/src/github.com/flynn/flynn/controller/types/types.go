27787f42711b44990cdab0925d021bc578922c2f
Matt Robinson <robinson.matty@gmail.com>
2015-12-14 01:51:20 -0500

    more godeps

---
+++ Godeps/_workspace/src/github.com/flynn/flynn/controller/types/types.go
@@ -0,0 +1,272 @@
+package types
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"time"
+
+	"lithiumreactor.com/internal/pgbackups/Godeps/_workspace/src/github.com/flynn/flynn/host/resource"
+	"lithiumreactor.com/internal/pgbackups/Godeps/_workspace/src/github.com/flynn/flynn/host/types"
+	"lithiumreactor.com/internal/pgbackups/Godeps/_workspace/src/github.com/flynn/flynn/pkg/tlscert"
+	"lithiumreactor.com/internal/pgbackups/Godeps/_workspace/src/github.com/flynn/flynn/router/types"
+)
+
+const RouteParentRefPrefix = "controller/apps/"
+
+type ExpandedFormation struct {
+	App       *App           `json:"app,omitempty"`
+	Release   *Release       `json:"release,omitempty"`
+	Artifact  *Artifact      `json:"artifact,omitempty"`
+	Processes map[string]int `json:"processes,omitempty"`
+	UpdatedAt time.Time      `json:"updated_at,omitempty"`
+}
+
+type App struct {
+	ID            string            `json:"id,omitempty"`
+	Name          string            `json:"name,omitempty"`
+	Meta          map[string]string `json:"meta"`
+	Strategy      string            `json:"strategy,omitempty"`
+	ReleaseID     string            `json:"release,omitempty"`
+	DeployTimeout int32             `json:"deploy_timeout,omitempty"`
+	CreatedAt     *time.Time        `json:"created_at,omitempty"`
+	UpdatedAt     *time.Time        `json:"updated_at,omitempty"`
+}
+
+func (a *App) System() bool {
+	v, ok := a.Meta["flynn-system-app"]
+	return ok && v == "true"
+}
+
+type Release struct {
+	ID         string                 `json:"id,omitempty"`
+	ArtifactID string                 `json:"artifact,omitempty"`
+	Env        map[string]string      `json:"env,omitempty"`
+	Meta       map[string]string      `json:"meta,omitempty"`
+	Processes  map[string]ProcessType `json:"processes,omitempty"`
+	CreatedAt  *time.Time             `json:"created_at,omitempty"`
+}
+
+type ProcessType struct {
+	Cmd         []string           `json:"cmd,omitempty"`
+	Entrypoint  []string           `json:"entrypoint,omitempty"`
+	Env         map[string]string  `json:"env,omitempty"`
+	Ports       []Port             `json:"ports,omitempty"`
+	Data        bool               `json:"data,omitempty"`
+	Omni        bool               `json:"omni,omitempty"` // omnipresent - present on all hosts
+	HostNetwork bool               `json:"host_network,omitempty"`
+	Service     string             `json:"service,omitempty"`
+	Resurrect   bool               `json:"resurrect,omitempty"`
+	Resources   resource.Resources `json:"resources,omitempty"`
+}
+
+type Port struct {
+	Port    int           `json:"port"`
+	Proto   string        `json:"proto"`
+	Service *host.Service `json:"service,omitempty"`
+}
+
+type Artifact struct {
+	ID        string     `json:"id,omitempty"`
+	Type      string     `json:"type,omitempty"`
+	URI       string     `json:"uri,omitempty"`
+	CreatedAt *time.Time `json:"created_at,omitempty"`
+}
+
+type Formation struct {
+	AppID     string         `json:"app,omitempty"`
+	ReleaseID string         `json:"release,omitempty"`
+	Processes map[string]int `json:"processes,omitempty"`
+	CreatedAt *time.Time     `json:"created_at,omitempty"`
+	UpdatedAt *time.Time     `json:"updated_at,omitempty"`
+}
+
+type Key struct {
+	ID        string     `json:"fingerprint,omitempty"`
+	Key       string     `json:"key,omitempty"`
+	Comment   string     `json:"comment,omitempty"`
+	CreatedAt *time.Time `json:"created_at,omitempty"`
+}
+
+type Job struct {
+	ID        string            `json:"id,omitempty"`
+	AppID     string            `json:"app,omitempty"`
+	ReleaseID string            `json:"release,omitempty"`
+	Type      string            `json:"type,omitempty"`
+	State     string            `json:"state,omitempty"`
+	Cmd       []string          `json:"cmd,omitempty"`
+	Meta      map[string]string `json:"meta,omitempty"`
+	CreatedAt *time.Time        `json:"created_at,omitempty"`
+	UpdatedAt *time.Time        `json:"updated_at,omitempty"`
+}
+
+type DomainMigration struct {
+	ID         string        `json:"id"`
+	OldTLSCert *tlscert.Cert `json:"old_tls_cert,omitempty"`
+	TLSCert    *tlscert.Cert `json:"tls_cert,omitempty"`
+	OldDomain  string        `json:"old_domain"`
+	Domain     string        `json:"domain"`
+	CreatedAt  *time.Time    `json:"created_at,omitempty"`
+	FinishedAt *time.Time    `json:"finished_at,omitempty"`
+}
+
+func (e *Job) IsDown() bool {
+	return e.State == "failed" || e.State == "crashed" || e.State == "down"
+}
+
+type JobEvents map[string]map[string]int
+
+type NewJob struct {
+	ReleaseID  string             `json:"release,omitempty"`
+	ReleaseEnv bool               `json:"release_env,omitempty"`
+	Cmd        []string           `json:"cmd,omitempty"`
+	Entrypoint []string           `json:"entrypoint,omitempty"`
+	Env        map[string]string  `json:"env,omitempty"`
+	Meta       map[string]string  `json:"meta,omitempty"`
+	TTY        bool               `json:"tty,omitempty"`
+	Columns    int                `json:"tty_columns,omitempty"`
+	Lines      int                `json:"tty_lines,omitempty"`
+	DisableLog bool               `json:"disable_log,omitempty"`
+	Resources  resource.Resources `json:"resources,omitempty"`
+}
+
+const DefaultDeployTimeout = 30 // seconds
+
+type Deployment struct {
+	ID            string         `json:"id,omitempty"`
+	AppID         string         `json:"app,omitempty"`
+	OldReleaseID  string         `json:"old_release,omitempty"`
+	NewReleaseID  string         `json:"new_release,omitempty"`
+	Strategy      string         `json:"strategy,omitempty"`
+	Status        string         `json:"status,omitempty"`
+	Processes     map[string]int `json:"processes,omitempty"`
+	DeployTimeout int32          `json:"deploy_timeout,omitempty"`
+	CreatedAt     *time.Time     `json:"created_at,omitempty"`
+	FinishedAt    *time.Time     `json:"finished_at,omitempty"`
+}
+
+type DeployID struct {
+	ID string
+}
+
+type DeploymentEvent struct {
+	AppID        string `json:"app,omitempty"`
+	DeploymentID string `json:"deployment,omitempty"`
+	ReleaseID    string `json:"release,omitempty"`
+	Status       string `json:"status,omitempty"`
+	JobType      string `json:"job_type,omitempty"`
+	JobState     string `json:"job_state,omitempty"`
+	Error        string `json:"error,omitempty"`
+}
+
+func (e *DeploymentEvent) Err() error {
+	if e.Error == "" {
+		return nil
+	}
+	return errors.New(e.Error)
+}
+
+type Provider struct {
+	ID        string     `json:"id,omitempty"`
+	URL       string     `json:"url,omitempty"`
+	Name      string     `json:"name,omitempty"`
+	CreatedAt *time.Time `json:"created_at,omitempty"`
+	UpdatedAt *time.Time `json:"updated_at,omitempty"`
+}
+
+type Resource struct {
+	ID         string            `json:"id,omitempty"`
+	ProviderID string            `json:"provider,omitempty"`
+	ExternalID string            `json:"external_id,omitempty"`
+	Env        map[string]string `json:"env,omitempty"`
+	Apps       []string          `json:"apps,omitempty"`
+	CreatedAt  *time.Time        `json:"created_at,omitempty"`
+}
+
+type ResourceReq struct {
+	ProviderID string           `json:"-"`
+	Apps       []string         `json:"apps,omitempty"`
+	Config     *json.RawMessage `json:"config"`
+}
+
+type ValidationError struct {
+	Field   string `json:"field,omitempty"`
+	Message string `json:"message"`
+}
+
+func (v ValidationError) Error() string {
+	return fmt.Sprintf("validation error: %s %s", v.Field, v.Message)
+}
+
+type NotFoundError struct {
+	Resource string `json:"field"`
+}
+
+func (n NotFoundError) Error() string {
+	return fmt.Sprintf("resource not found: %s", n.Resource)
+}
+
+type LogOpts struct {
+	Follow      bool
+	JobID       string
+	Lines       *int
+	ProcessType *string
+}
+
+type EventType string
+
+const (
+	EventTypeApp              EventType = "app"
+	EventTypeAppDeletion      EventType = "app_deletion"
+	EventTypeAppRelease       EventType = "app_release"
+	EventTypeDeployment       EventType = "deployment"
+	EventTypeJob              EventType = "job"
+	EventTypeScale            EventType = "scale"
+	EventTypeRelease          EventType = "release"
+	EventTypeArtifact         EventType = "artifact"
+	EventTypeProvider         EventType = "provider"
+	EventTypeResource         EventType = "resource"
+	EventTypeResourceDeletion EventType = "resource_deletion"
+	EventTypeKey              EventType = "key"
+	EventTypeKeyDeletion      EventType = "key_deletion"
+	EventTypeRoute            EventType = "route"
+	EventTypeRouteDeletion    EventType = "route_deletion"
+	EventTypeDomainMigration  EventType = "domain_migration"
+)
+
+type Event struct {
+	ID         int64           `json:"id,omitempty"`
+	AppID      string          `json:"app,omitempty"`
+	ObjectType EventType       `json:"object_type,omitempty"`
+	ObjectID   string          `json:"object_id,omitempty"`
+	UniqueID   string          `json:"-"`
+	Data       json.RawMessage `json:"data,omitempty"`
+	CreatedAt  *time.Time      `json:"created_at,omitempty"`
+}
+
+type Scale struct {
+	PrevProcesses map[string]int `json:"prev_processes,omitempty"`
+	Processes     map[string]int `json:"processes"`
+	ReleaseID     string         `json:"release"`
+}
+
+type AppRelease struct {
+	PrevRelease *Release `json:"prev_release,omitempty"`
+	Release     *Release `json:"release"`
+}
+
+type AppDeletion struct {
+	AppID            string          `json:"app"`
+	DeletedRoutes    []*router.Route `json:"deleted_routes"`
+	DeletedResources []*Resource     `json:"deleted_resources"`
+}
+
+type AppDeletionEvent struct {
+	AppDeletion *AppDeletion `json:"app_deletion"`
+	Error       string       `json:"error"`
+}
+
+type DomainMigrationEvent struct {
+	DomainMigration *DomainMigration `json:"domain_migration"`
+	Error           string           `json:"error,omitempty"`
+}
---
+++ Godeps/_workspace/src/github.com/flynn/flynn/pkg/httphelper/request_logger.go
@@ -0,0 +1,38 @@
+package httphelper
+
+import (
+	"net"
+	"net/http"
+	"strings"
+	"time"
+
+	log "lithiumreactor.com/internal/pgbackups/Godeps/_workspace/src/gopkg.in/inconshreveable/log15.v2"
+	"lithiumreactor.com/internal/pgbackups/Godeps/_workspace/src/github.com/flynn/flynn/pkg/ctxhelper"
+)
+
+func NewRequestLogger(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		rw := w.(*ResponseWriter)
+
+		reqID, _ := ctxhelper.RequestIDFromContext(rw.Context())
+		componentName, _ := ctxhelper.ComponentNameFromContext(rw.Context())
+		logger := log.New(log.Ctx{"component": componentName, "req_id": reqID})
+		rw.ctx = ctxhelper.NewContextLogger(rw.Context(), logger)
+
+		start := time.Now()
+		var clientIP string
+		clientIPs := strings.Split(req.Header.Get("X-Forwarded-For"), ",")
+		if len(clientIPs) > 0 {
+			clientIP = strings.TrimSpace(clientIPs[len(clientIPs)-1])
+		}
+		if clientIP == "" {
+			clientIP, _, _ = net.SplitHostPort(req.RemoteAddr)
+		}
+
+		logger.Info("request started", "method", req.Method, "path", req.URL.Path, "client_ip", clientIP)
+
+		handler.ServeHTTP(rw, req)
+
+		logger.Info("request completed", "status", rw.Status(), "duration", time.Since(start))
+	})
+}
